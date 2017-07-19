374b431c10de1cbaa163a674cd083e17c1ab506c
svaduka <sainagaraju.vaduka@gmail.com>
2017-07-19 07:43:20 +0530

    Created Java Class HDFSService

---
+++ Batch040717/src/com/laboros/HDFSService.java
@@ -0,0 +1,21 @@
+package com.laboros;
+
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.util.Tool;
+
+public class HDFSService extends Configured implements Tool 
+{
+
+	public static void main(String[] args) {
+
+		//Validations
+	}
+	
+	@Override
+	public int run(String[] args) throws Exception 
+	{
+		//logic to connect hdfs and write data to hdfs
+		return 0;
+	}
+
+}
