0c04a1edcd11d0e7d8456b1706344b473ae76202
Kiran Reddy <kiranrredy.a@gmail.com>
2017-07-20 14:54:18 +0530

    HDFSService validations added.

--- Batch040717/src/com/laboros/HDFSService.java
+++ Batch040717/src/com/laboros/HDFSService.java
@@ -1,16 +1,36 @@
 package com.laboros;
 
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.util.Tool;
 
 public class HDFSService extends Configured implements Tool 
 {
 
+	//Java Usage: HDFSService /path/to/edge/node/local/file /path/to/hdfs/destination/directory
 	public static void main(String[] args) {
 
 		//Validations
+		if(args.length < 2){
+			System.out.println("Invalid no.of arguments");
+			return;
+		}
+		
+		Configuration config = new Configuration();
+		printConfig(config);
 	}
 	
+	private static void printConfig(Configuration config) {
+		
+		for(Map.Entry<String, String> entry : config){
+			System.out.println(entry.getKey()+" = "+entry.getValue());
+		}
+		
+	}
+
 	@Override
 	public int run(String[] args) throws Exception 
 	{
