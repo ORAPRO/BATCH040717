455917c999b9e71e0e62a716ef56e5be4f0f840c
svaduka <sainagaraju.vaduka@gmail.com>
2017-07-24 07:37:41 +0530

    Created HDFSService hdfs.create(Path)

--- Batch040717/src/com/laboros/HDFSService.java
+++ Batch040717/src/com/laboros/HDFSService.java
@@ -2,6 +2,9 @@ package com.laboros;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
@@ -20,7 +23,7 @@ public class HDFSService extends Configured implements Tool
 		}
 		
 		Configuration conf = new Configuration(Boolean.TRUE);
-		
+		conf.set("fs.defaultFS", "hdfs://localhost:8020");
 		
 		try {
 			int returnValue=ToolRunner.run(conf, new HDFSService(), args);
@@ -43,6 +46,30 @@ public class HDFSService extends Configured implements Tool
 	public int run(String[] args) throws Exception 
 	{
 		//logic to connect hdfs and write data to hdfs
+		
+		//step:1 : Get the configuraiton object
+		
+		Configuration conf=super.getConf();
+		
+		//step:2 Creating the file system object
+		
+		FileSystem hdfs = FileSystem.get(conf);
+		
+		//step:3 Read the input arguments
+		
+		final String edgeNodeLocalFileName=args[0];// WordCount.txt
+		final String hdfsDestLoc=args[1];
+		
+		//Convert to URI
+		//step: 4: Create the path, because hdfs will always understand the file locations as URI
+		Path hdfsPath= new Path(hdfsDestLoc, edgeNodeLocalFileName);
+		
+		//step: 5: Invoke filesytem.create
+		
+		FSDataOutputStream fsdos=hdfs.create(hdfsPath);
+		
+		
+		
 		return 0;
 	}
 
