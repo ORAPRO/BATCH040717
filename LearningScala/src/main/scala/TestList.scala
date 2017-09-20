f0c20dbc1893c2b14027d5c0f2c03050eb774861
svaduka <sainagaraju.vaduka@gmail.com>
2017-09-20 07:43:17 +0530

    Added List

--- LearningScala/src/main/scala/TestList.scala
+++ LearningScala/src/main/scala/TestList.scala
@@ -5,13 +5,29 @@ object TestList {
     
     //Creating a list object
     
+    
     val l = Nil;
     
     var l1 = List();
     
-    var l2 = List(1,2,3,4,5,6,7);
+    val l2 = List(1,2,3,4,5,6,7);
+    
+    val numbers = 10 :: (20 :: (30 :: (40 :: Nil)))
+    
+    println("HEAD"+numbers.head);
+    
+    println("TAIL:"+numbers.tail);
+    
+    println(numbers.take(3));
     
     println(l2.size);
+    
+    val l3:List[Int] = List(4,5,6,7,8);
+    
+    l2:::l3
+    
+    println(l2.size);
+    
     
     for(j <- l2)
     {
